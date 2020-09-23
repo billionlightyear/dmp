6a730d22ef188520196d698c9034760d74de5db6
LinY <2471577325@qq.com>
2020-09-23 14:40:01 +0800

    文件格式转换

---
+++ src/main/scala/com/lin/young/etl/Log2Parquet.scala
@@ -0,0 +1,14 @@
+package com.lin.young.etl
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 
+ * User: LinY
+ * Date: 2020-09-23
+ * Time: 14:36
+ */
+object Log2Parquet {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
