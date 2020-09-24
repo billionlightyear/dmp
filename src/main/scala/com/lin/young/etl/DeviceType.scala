65fc42b5b7b259c12c629c28d1b63fb3c071701a
LinY <2471577325@qq.com>
2020-09-24 21:47:21 +0800

    设备类型

---
+++ src/main/scala/com/lin/young/etl/DeviceType.scala
@@ -0,0 +1,29 @@
+package com.lin.young.etl
+
+import com.lin.young.util.RowUtils
+import org.apache.spark.sql.SparkSession
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 
+ * User: LinY
+ * Date: 2020-09-24
+ * Time: 21:46
+ */
+object DeviceType {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local").appName(this.getClass.getName).getOrCreate()
+
+    val df = spark.read.load("output/part-00000-f03b3a71-f093-4f4d-ae5e-5e7a8f44a223-c000.gz.parquet")
+
+    df.rdd.map(row => {
+      (row.getAs[String]("devicetype"), RowUtils.countUtils(row))
+    })
+      .reduceByKey((list1, list2) => {
+        list1.zip(list2).map(t => {
+          t._1 + t._2
+        })
+      }).take(10)
+      .foreach(println)
+  }
+}
